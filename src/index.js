5daee7ed2ff40183cfc2bf98a20f20ea2ba230fb
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2023-01-10 21:49:25 +0200

    c

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BreakpointProvider } from 'react-socks';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import './reset.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BreakpointProvider>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </BreakpointProvider>
   </React.StrictMode>
 );
