bfe562699815c0532fd493bbb379a884195f0051
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2023-04-07 11:28:32 +0300

    changes

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Modal from '../modal/Modal.js';
-import { useToggle } from '../customHooks/CustomHooks.js';
+import { useToggle } from '../../customHooks/CustomHooks.js';
 import validator from 'validator';
 import {
   MyForm,
